11f269ddc882c9471abb17a46f1cda6813fcdb6a
Ariane Souza <ari.souza02@outlook.com>
2026-03-09 20:40:30 -0300

    feat: Tela de autocadastro

--- frontend/internet-banking/src/app/app.routes.ts
+++ frontend/internet-banking/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout';
+import { CadastroComponent } from './pages/cadastro/cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: CadastroComponent
+      }
+    ]
+  }
+
+];
