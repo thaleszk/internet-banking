e0fd6f4da29d4c84c1e059048e3b7ebffa4a7cdd
leopamps <leo.pamplona96@hotmail.com>
2026-03-10 17:20:04 -0300

    feat: dir backend com spring boot

---
+++ backend/src/main/java/com/bantads/demo/BatadsApplication.java
@@ -0,0 +1,13 @@
+package com.bantads.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatadsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bantads/demo/BatadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bantads.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
