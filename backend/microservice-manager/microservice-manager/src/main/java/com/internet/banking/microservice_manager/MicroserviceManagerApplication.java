076bdbe984beaaf95a9304f5115f356dd0845168
leopamps <leo.pamplona96@hotmail.com>
2026-04-14 17:25:22 -0300

    feat: add microservice-manager

---
+++ backend/microservice-manager/microservice-manager/src/main/java/com/internet/banking/microservice_manager/MicroserviceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.internet.banking.microservice_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceManagerApplication.class, args);
+	}
+
+}
---
+++ backend/microservice-manager/microservice-manager/src/test/java/com/internet/banking/microservice_manager/MicroserviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internet.banking.microservice_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
